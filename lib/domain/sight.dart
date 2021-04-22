035ebaa8063fc73391a47e488f95e42d737e3ace
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2021-04-23 01:04:06 +0500

    changed naming for variables in models

--- lib/domain/sight.dart
+++ lib/domain/sight.dart
@@ -4,16 +4,16 @@
 class Sight {
   const Sight({
     required this.name,
-    this.lat,
-    this.lon,
+    this.latitude,
+    this.longitude,
     this.url,
     this.details,
     this.type
   });
 
   final String name;
-  final double? lat;
-  final double? lon;
+  final double? latitude;
+  final double? longitude;
   final String? url;
   final String? details;
   final String? type;
